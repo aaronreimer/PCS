0af7860f36d6fed75785f2aeb29b052478dc8960
aaronreimer <aaronreimer02@gmail.com>
2019-09-26 12:29:24 -0700

    initial commit

---
+++ PCS/SetupForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCS
+{
+    public partial class SetupForm : Form
+    {
+        public SetupForm()
+        {
+            InitializeComponent();
+        }
+
+        public static int size, seed;
+
+        private void TxtSize_TextChanged(object sender, EventArgs e)
+        {
+            lblSize.Text = "x " + txtSize.Text;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                size = int.Parse(txtSize.Text);
+            }
+            catch (Exception)
+            {
+                size = 5;
+            }
+            
+            try
+            {
+                seed = int.Parse(txtSeed.Text);
+            }
+            catch (Exception)
+            {
+                Random r = new Random();
+                seed = r.Next();
+            }
+            Game ng = new Game();
+            ng.Show();
+            
+            
+            
+        }
+    }
+}
