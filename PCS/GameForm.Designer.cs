41d9b9eaff8eaeb879ad1d5cc63de4e8f1282352
aaronreimer <aaronreimer02@gmail.com>
2019-10-02 09:43:22 -0700

    made changes ------------------------------------------------------- 2 windows -> 1 window added show solution add time mode which doesnt work

---
+++ PCS/GameForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PCS
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.chkTimeAttack = new System.Windows.Forms.CheckBox();
+            this.btnBest = new System.Windows.Forms.Button();
+            this.pnlSetupControls = new System.Windows.Forms.Panel();
+            this.pnlGamePanel = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnSolution = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.tmrGameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pnlSetupControls.SuspendLayout();
+            this.pnlGamePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(67, 8);
+            this.txtSize.MaxLength = 2;
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(22, 20);
+            this.txtSize.TabIndex = 1;
+            this.txtSize.TextChanged += new System.EventHandler(this.TxtSize_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 104);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(67, 34);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(129, 20);
+            this.txtSeed.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seed:";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(92, 11);
+            this.lblSize.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(12, 13);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "x";
+            // 
+            // chkTimeAttack
+            // 
+            this.chkTimeAttack.AutoSize = true;
+            this.chkTimeAttack.Location = new System.Drawing.Point(14, 60);
+            this.chkTimeAttack.Name = "chkTimeAttack";
+            this.chkTimeAttack.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chkTimeAttack.Size = new System.Drawing.Size(85, 17);
+            this.chkTimeAttack.TabIndex = 6;
+            this.chkTimeAttack.Text = "Timed Mode";
+            this.chkTimeAttack.UseVisualStyleBackColor = true;
+            // 
+            // btnBest
+            // 
+            this.btnBest.Location = new System.Drawing.Point(121, 104);
+            this.btnBest.Name = "btnBest";
+            this.btnBest.Size = new System.Drawing.Size(75, 23);
+            this.btnBest.TabIndex = 7;
+            this.btnBest.Text = "Best Times";
+            this.btnBest.UseVisualStyleBackColor = true;
+            this.btnBest.Visible = false;
+            this.btnBest.Click += new System.EventHandler(this.BtnBest_Click);
+            // 
+            // pnlSetupControls
+            // 
+            this.pnlSetupControls.Controls.Add(this.label1);
+            this.pnlSetupControls.Controls.Add(this.txtSize);
+            this.pnlSetupControls.Controls.Add(this.btnBest);
+            this.pnlSetupControls.Controls.Add(this.txtSeed);
+            this.pnlSetupControls.Controls.Add(this.chkTimeAttack);
+            this.pnlSetupControls.Controls.Add(this.label2);
+            this.pnlSetupControls.Controls.Add(this.lblSize);
+            this.pnlSetupControls.Controls.Add(this.btnStart);
+            this.pnlSetupControls.Location = new System.Drawing.Point(12, 12);
+            this.pnlSetupControls.Name = "pnlSetupControls";
+            this.pnlSetupControls.Size = new System.Drawing.Size(217, 152);
+            this.pnlSetupControls.TabIndex = 9;
+            // 
+            // pnlGamePanel
+            // 
+            this.pnlGamePanel.Controls.Add(this.btnBack);
+            this.pnlGamePanel.Controls.Add(this.btnSolution);
+            this.pnlGamePanel.Controls.Add(this.lblTimer);
+            this.pnlGamePanel.Location = new System.Drawing.Point(12, 12);
+            this.pnlGamePanel.Name = "pnlGamePanel";
+            this.pnlGamePanel.Size = new System.Drawing.Size(353, 366);
+            this.pnlGamePanel.TabIndex = 11;
+            this.pnlGamePanel.Visible = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(3, 27);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // btnSolution
+            // 
+            this.btnSolution.Location = new System.Drawing.Point(3, 3);
+            this.btnSolution.Name = "btnSolution";
+            this.btnSolution.Size = new System.Drawing.Size(75, 23);
+            this.btnSolution.TabIndex = 3;
+            this.btnSolution.Text = "Solution";
+            this.btnSolution.UseVisualStyleBackColor = true;
+            this.btnSolution.Click += new System.EventHandler(this.BtnSolution_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(84, 8);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(57, 13);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "Time: 0:00";
+            // 
+            // tmrGameTimer
+            // 
+            this.tmrGameTimer.Enabled = true;
+            this.tmrGameTimer.Interval = 1000;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 403);
+            this.Controls.Add(this.pnlGamePanel);
+            this.Controls.Add(this.pnlSetupControls);
+            this.Name = "GameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Setup";
+            this.pnlSetupControls.ResumeLayout(false);
+            this.pnlSetupControls.PerformLayout();
+            this.pnlGamePanel.ResumeLayout(false);
+            this.pnlGamePanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.CheckBox chkTimeAttack;
+        private System.Windows.Forms.Button btnBest;
+        private System.Windows.Forms.Panel pnlSetupControls;
+        private System.Windows.Forms.Panel pnlGamePanel;
+        private System.Windows.Forms.Button btnSolution;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer tmrGameTimer;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ PCS/GameForm.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PCS
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        int size, seed, seconds, minutes;
+        public static bool timed;
+        static int[,] guess, solution;
+        Random rand;
+        List<String> rowList, colList;
+        
+
+        private void TxtSize_TextChanged(object sender, EventArgs e)
+        {
+            lblSize.Text = "x " + txtSize.Text;
+        }
+
+        private void BtnBest_Click(object sender, EventArgs e)
+        {
+            /* pnlTimes.Visible = true;
+             pnlSetupControls.Visible = false;
+             pnlTimes.Left = 0;
+             pnlTimes.Top = 0;
+
+             StreamReader sr = new StreamReader("times.txt");
+             string line;
+             while (!sr.EndOfStream)
+             {
+                 line = sr.ReadLine();
+                 if (line.StartsWith("z"))
+                 {
+                     if (tbcBestTimes.TabPages.ContainsKey(line[1] + "x" + line[1]))
+                     {
+
+                     }
+                     else
+                     {
+                         tbcBestTimes.TabPages.Add(line[1] + "x" + line[1]);
+                     }
+
+
+                 }
+             }*/
+        }
+
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+
+            foreach (Label l in pnlGamePanel.Controls.OfType<Label>())
+            {
+                l.Visible = false;
+            }
+            foreach (Box b  in pnlGamePanel.Controls.OfType<Box>())
+            {
+                b.Visible = false;
+            }
+            try
+            {
+                size = int.Parse(txtSize.Text);
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+
+                seed = int.Parse(txtSeed.Text);
+                seed = txtSeed.Text.GetHashCode();
+
+            }
+            catch (Exception)
+            {
+                Random r = new Random();
+                seed = r.Next();
+
+            }
+
+            timed = chkTimeAttack.Checked;
+            rand = new Random(seed);
+            pnlGamePanel.Visible = true;
+            pnlSetupControls.Visible = false;
+            lblTimer.Visible = GameForm.timed;
+            tmrGameTimer.Enabled = GameForm.timed;
+
+            solution = new int[size, size];
+            guess = new int[size, size];
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    guess[i, j] = 3;
+                    Box box = new Box();
+                    box.Height = 20;
+                    box.Width = 20;
+                    box.Top = 100 + (20 * i);
+                    box.Left = 100 + (20 * j);
+
+                    box.MouseDown += Box_Click;
+                    box.BorderStyle = BorderStyle.FixedSingle;
+                    box.BackColor = Color.Gray;
+                    box.row = i;
+                    box.col = j;
+                    pnlGamePanel.Controls.Add(box);
+                    int result = rand.Next(0, 3);
+                    if (result == 0)
+                    {
+                        solution[i, j] = 0;
+                        // box.BackColor = Color.White;
+
+                    }
+                    else
+                    {
+                        solution[i, j] = 1;
+                        //  box.BackColor = Color.Black;
+
+                    }
+
+                }
+
+                //row numbers
+                string row = "";
+
+                for (int j = 0; j < size; j++)
+                {
+                    if (j == 0 && solution[i, j] == 0 || j == size - 1 && solution[i, j] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        row += solution[i, j];
+                    }
+
+                }
+
+                rowList = new List<String>();
+
+
+                rowList = row.Split('0').ToList();
+                rowList.RemoveAll(item => item == "");
+
+
+
+                for (int k = 0; k < rowList.Count; k++)
+                {
+                    Label sideLabel = new Label();
+                    sideLabel.Width = 15;
+                    sideLabel.Height = 20;
+                    sideLabel.Top = 102 + 20 * i;
+                    sideLabel.Left = 105 - rowList.Count * 20 + 20 * k;
+                    sideLabel.Text = "0";
+                    
+                    sideLabel.Text = rowList[k].Length.ToString();
+                    
+                    pnlGamePanel.Controls.Add(sideLabel);
+                }
+            }
+
+            //col numbers
+            for (int i = 0; i < size; i++)
+            {
+
+                string col = "";
+                for (int j = 0; j < size; j++)
+                {
+                    if (j == 0 && solution[j, i] == 0 || j == size - 1 && solution[j, i] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        col += solution[j, i];
+                    }
+                }
+                colList = new List<String>();
+               
+                colList = col.Split('0').ToList();
+                colList.RemoveAll(item => item == "");
+            
+                for (int k = 0; k < colList.Count; k++)
+                {
+
+                    Label sideLabel = new Label();
+                    sideLabel.Width = 20;
+                    sideLabel.Height = 20;
+                    sideLabel.Top = 105 - colList.Count * 20 + 20 * k;
+                    sideLabel.Left = 102 + 20 * i;
+
+                    string a = "1" + colList[k].Length.ToString();
+                    if (a.Contains("1"))
+                    {
+                        sideLabel.Text = "0";
+                    }
+                    else
+                    {
+                        sideLabel.Text = colList[k].Length.ToString();
+                    }
+               
+                        
+                
+                    
+                    pnlGamePanel.Controls.Add(sideLabel);
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Debug.Write(solution[i, j]);
+                }
+            }
+            Debug.Write("\n");
+
+        }
+
+
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            pnlGamePanel.Visible = false;
+            pnlSetupControls.Visible = true;
+        }
+
+        private void Box_Click(object sender, EventArgs e)
+        {
+            Box boxClicked = (Box)sender;
+
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+
+            if (mouseEvent.Button == MouseButtons.Left)
+            {
+                if (boxClicked.BackColor == Color.Black)
+                {
+                    boxClicked.BackColor = Color.Gray;
+                    guess[boxClicked.row, boxClicked.col] = 3;
+                }
+                else
+                {
+                    boxClicked.BackColor = Color.Black;
+                    guess[boxClicked.row, boxClicked.col] = 1;
+                }
+            }
+            else if (mouseEvent.Button == MouseButtons.Right)
+            {
+                if (boxClicked.BackColor == Color.White)
+                {
+                    boxClicked.BackColor = Color.Gray;
+                    guess[boxClicked.row, boxClicked.col] = 3;
+                }
+                else
+                {
+                    boxClicked.BackColor = Color.White;
+                    guess[boxClicked.row, boxClicked.col] = 0;
+                }
+            }
+
+
+            if (checkSolution() == true)
+            {
+                LockBoard();
+                if (GameForm.timed)
+                {
+
+                    try
+                    {
+                        Debug.WriteLine(size + minutes + seconds);
+                        StreamWriter sw = new StreamWriter("times.txt", true);
+                        sw.WriteLine("z" + size);
+                        sw.WriteLine("m" + minutes);
+                        sw.WriteLine("s" + seconds);
+                        sw.Close();
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+
+                MessageBox.Show("You win");
+
+
+            }
+        }
+
+        private void TmrGameTimer_Tick(object sender, EventArgs e)
+        {
+            if (seconds > 59)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            if (seconds < 10)
+            {
+                lblTimer.Text = "Time: " + minutes + ":0" + seconds++;
+            }
+            else
+            {
+                lblTimer.Text = "Time: " + minutes + ":" + seconds++;
+            }
+        }
+
+        private void BtnSolution_Click(object sender, EventArgs e)
+        {
+            foreach (Box box in pnlGamePanel.Controls.OfType<Box>())
+            {
+                if (solution[box.row, box.col] == 1)
+                {
+                    box.BackColor = Color.Black;
+                }
+                else
+                {
+                    box.BackColor = Color.White;
+                }
+            }
+            LockBoard();
+        }
+
+        private Boolean checkSolution()
+        {
+            int correct = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (guess[i, j] == solution[i, j])
+                    {
+                        correct++;
+
+                    }
+                }
+            }
+            if (correct == size * size)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        private void LockBoard()
+        {
+            tmrGameTimer.Stop();
+            foreach (Box b in pnlGamePanel.Controls.OfType<Box>())
+            {
+                b.Enabled = false;
+
+            }
+
+        }
+    }
+}
+
